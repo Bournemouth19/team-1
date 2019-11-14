a170fea8e0b1202cc5c1e0897c583629c5fd74c8
mochipancakes <57260257+mochipancakes@users.noreply.github.com>
2019-11-14 15:25:47 +0000

    Update main.py
    corrected file names.

--- main.py
+++ main.py
@@ -8,33 +8,33 @@ import codecs
 def hello():
 	return readHTML('index.html')
 
-	
+
 @app.route("/search/<name>")
 def search(name):
 	path = 'LocationPages/'
 	if name in 'Cafe Area':
-		return readHTML(path + 'Cafe.html')
+		return readHTML(path + 'cafe.html')
 	elif name in 'Game Room':
-		return readHTML(path + 'Games_room.html')
+		return readHTML(path + 'games_room.html')
 	elif name in 'Hall way':
-		return readHTML(path + 'Hall.html')
+		return readHTML(path + 'hall.html')
 	elif name in 'Outside':
-		return readHTML(path + 'Outside.html')
+		return readHTML(path + 'outside.html')
 	elif name in 'Repair room':
 		return readHTML(path + 'PC_repair_room.html')
-	
+
 
 def readHTML(file):
 	f = codecs.open(file, 'r')
 	content = f.read()
 	return content;
-	
+
 def search_1(search_word):
 	video_dir = 'videos'
 	for filename in os.listdir(video_dir):
 		if search_word in filename:
 			pass # Do sthing
-	
+
 if __name__ == "__main__":
     app.run()
 	#print(readHTML('index.html'))
